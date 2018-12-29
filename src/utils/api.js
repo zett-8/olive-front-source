ff035f23e7b47799a418e256c0c3ccb4df42f7cb
Zett-8 <kame0008@gmail.com>
2018-12-29 10:32:42 +0900

    fix api path

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,13 +10,13 @@ const PATH = {
 
 export default {
   login: (email, password) => axios.post(`${PATH[ENV]}-token-auth/`, { username: email, password }),
-  buyWork: (buyerId, workId) => axios.patch(`${PATH[ENV]}/works/${workId}/`, { sold: true, buyer: buyerId }),
-  getWorks: () => axios.get(`${PATH[ENV]}/works/`),
-  getWorkDetail: id => axios.get(`${PATH[ENV]}/works/${id}/`),
-  getWorksOfAnArtist: id => axios.get(`${PATH[ENV]}/works/?artist=${id}`),
-  getMessages: workId => axios.get(`${PATH[ENV]}/messages/?work=${workId}`),
+  buyWork: (buyerId, workId) => axios.patch(`${PATH[ENV]}/v1/works/${workId}/`, { sold: true, buyer: buyerId }),
+  getWorks: () => axios.get(`${PATH[ENV]}/v1/works/`),
+  getWorkDetail: id => axios.get(`${PATH[ENV]}/v1/works/${id}/`),
+  getWorksOfAnArtist: id => axios.get(`${PATH[ENV]}/v1/works/?artist=${id}`),
+  getMessages: workId => axios.get(`${PATH[ENV]}/v1/messages/?work=${workId}`),
   sendMessage: (workId, sender, receiver, body) =>
-    axios.post(`${PATH[ENV]}/messages/`, { work: workId, sender, receiver, body }),
-  getUserDetail: id => axios.get(`${PATH[ENV]}/users/${id}/`),
-  getArtistDetail: id => axios.get(`${PATH[ENV]}/artists/${id}/`),
+    axios.post(`${PATH[ENV]}/v1/messages/`, { work: workId, sender, receiver, body }),
+  getUserDetail: id => axios.get(`${PATH[ENV]}/v1/users/${id}/`),
+  getArtistDetail: id => axios.get(`${PATH[ENV]}/v1/artists/${id}/`),
 }

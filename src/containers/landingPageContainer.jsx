9c71c7385254fe9a1752cf5e5a9028830e771183
Zett-8 <kame0008@gmail.com>
2019-04-11 16:33:31 +0900

    added new landing page

--- src/containers/landingPageContainer.jsx
+++ src/containers/landingPageContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import Olive from '../assets/olive.jpg'
+
 class LandingPageContainer extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h2>Welcome to Olive</h2>
+        <p style={{ textAlign: 'center'}}><img src={Olive} alt="" style={{ margin: '0 auto', width: '80vw'}} /></p>
       </React.Fragment>
     )
   }

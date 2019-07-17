bef60c3638a9d2e1164867bdeac955e04e9ce722
Zett-8 <kame0008@gmail.com>
2019-07-17 19:43:10 +0900

    set google tag manager module

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,21 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
 import store, { persistor } from './reducers/store'
-
 import Router from './router'
-
 import './styles/main.scss'
+
+import TagManager from 'react-gtm-module'
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('olive')
+
+  const tagManagerArgs = {
+    gtmId: 'GTM-WD5QC5V'
+  }
+
+  TagManager.initialize(tagManagerArgs)
+}
+
 
 if (process.env.NODE_ENV !== 'production') console.log(`==== connecting to ${process.env.NODE_ENV} server ====`)
 

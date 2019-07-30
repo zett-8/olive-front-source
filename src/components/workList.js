8fb79bd09913834453820628fb05ce3ed140ec6a
Zett-8 <kame0008@gmail.com>
2019-07-30 12:49:32 +0900

    fix css to only show one work

--- src/components/workList.js
+++ src/components/workList.js
@@ -5,20 +5,36 @@ import PropTypes from 'prop-types'
 import VOID from '../assets/700.gif'
 
 const workList = props => {
+  const workListRenderer = () => (
+    props.works.length === 1 ? (
+      <div className="oneWorkList">
+        {props.works.map(w => (
+          <div className="work" key={w.id}>
+            <Link key={w.id} to={`/work/${w.id}/detail`}>
+              <img alt="" src={VOID} style={{ backgroundImage: `url(${w.image1})` }} />
+            </Link>
+            <p>{w.title}</p>
+          </div>
+        ))}
+      </div>
+    ) : (
+      <div className="workList">
+        {props.works.map(w => (
+          <div className="work" key={w.id}>
+            <Link key={w.id} to={`/work/${w.id}/detail`}>
+              <img alt="" src={VOID} style={{ backgroundImage: `url(${w.image1})` }} />
+            </Link>
+            <p>{w.title}</p>
+          </div>
+        ))}
+      </div>
+    )
+  )
+
   return (
     <React.Fragment>
       {props.works.length ? (
-        <div className="workList">
-          {props.works.map(w => (
-            <div className="work" key={w.id}>
-              <Link key={w.id} to={`/work/${w.id}/detail`}>
-                <img alt="" src={VOID} style={{ backgroundImage: `url(${w.image1})` }} />
-              </Link>
-              <p>{w.title}</p>
-            </div>
-          ))}
-        </div>
-
+        workListRenderer()
       ) : (
         <div className="noWorkList">
           <p className="noWorks">作品がありません</p>
